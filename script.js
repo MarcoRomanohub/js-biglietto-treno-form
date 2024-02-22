382785019b60a46e549eb3f337c3e7e2545fa3c1
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-02-22 10:44:37 +0100

    [add] reset

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 // INPUT
+// FIXME: Potevo direttamente salvare così: const passengerId = document.querySelector('.input1').value; ma all'interno della funzione "click", e avevo finito
 let input1 = document.querySelector('.input1');
 let input2 = document.querySelector('.input2');
 let input3 = document.getElementById('eta');
@@ -16,18 +17,24 @@ let km;
 const priceXKm = 0.21;
 let ticketPrice = km * priceXKm;
 let discount;
+let tipoBiglietto;
 
 
 btn1.addEventListener('click',function(event){
   event.preventDefault();
+  // const km = parseInt(document.querySelector('.input2').value)
   km = input2.value;
   passengerId = input1.value
   ticketPrice = km * priceXKm;
 
   if(input3.value == 3){
     discount = ticketPrice * 0.4;
+    tipoBiglietto = 'Sconto Senior'
   }else if(input3.value == 1){
     discount = ticketPrice * 0.2;
+    tipoBiglietto = 'Sconto Minorenne'
+  }else{
+    tipoBiglietto = 'Biglietto Standard'
   }
 
   if(input3.value != 2){
@@ -38,9 +45,14 @@ btn1.addEventListener('click',function(event){
   output.classList.remove('d-none')
 
   document.getElementById('idPassenger').innerHTML = passengerId;
+  document.getElementById('tipoBiglietto').innerHTML = tipoBiglietto;
   document.getElementById('randomCarriage').innerHTML = carriageNumber;
   document.getElementById('randomCp').innerHTML = codeCp;
   document.getElementById('outputPrice').innerHTML = ticketPrice.toFixed(2) + '€';
+})
+
+btn2.addEventListener('click',function(){
+  output.classList.add('d-none');
 })
 
 
